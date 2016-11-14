a4404d6b90f76282b87158516e213a00038c0353
CristinaHainberger <CristinaHainberger@10.201.92.233>
2016-11-14 11:24:57 +0100

    changed <h3>

--- SPEZ-UE/index.php
+++ SPEZ-UE/index.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>TEST DOUBLE PUSH</title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Home</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+      <!-- jQuery  -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="res/js/bootstrap.js" type="text/javascript"></script>
+
+  </head>
+  <body>
+   
+      <h3>Hi Ioulia</h3>
+
+  
+  </body>
 </html>
