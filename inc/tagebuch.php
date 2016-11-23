08a3403fab44500c4d9cb45872745b75cf6e2292
I-Kon <wd15k003@technikum-wien.at>
2016-11-23 13:46:52 +0100

    small changes (relative links, title)

--- inc/tagebuch.php
+++ inc/tagebuch.php
@@ -1,1 +1,8 @@
 <h5>TAGEBUCH Content</h5>
+
+<button type="button" id="saveBtn" class="btn btn-default">Speichern</button>
+<button type="button" id="breakBtn" class="btn btn-default">Abbrechen</button>
+
+<button type="button" class="btn btn-default txtBtn" value="Hallo das ist mein Text">Set own text with txtBtn</button>
+
+
--- inc/text_banner.php
+++ inc/text_banner.php
@@ -1,2 +1,23 @@
-<h5>Das ist der Platz für den Text_Banner. Wird grün wenn gespeichert, rot wenn nicht gespeichert.</h5>
+<?php
+if (isset($_GET['content'])) {
+    switch ($_GET['content']) {
+        case 'tagebuch':
+            echo "Haben Sie Ihr Medikament eingenommen?";
+            break;
+        case 'nebenwirkungen':
+            echo "Haben Sie Nebenwirkungen?";
+            break;
+        case 'behandlung':
+            echo "Ihre Einstellungen zu Medikamente und Behandlung";
+            break;
+        case 'bericht':
+            echo "Wechselwirkung zwischen Medikament und Nebenwirkung";
+            break;
+        default:
+            echo "Haben Sie Ihr Medikament eingenommen?";
+    }
+} else {
+    echo "Haben Sie Ihr Medikament eingenommen?";
+}
+?>             
 
